3f85fe64580b822efe7e91a012832e84e49a65f7
Joses Syanwa <josesyanwa@gmail.com>
2023-12-14 12:23:41 +0300

    Completed

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,15 @@
 def write_file(file_name, file_content):
-    pass
+    """Write file_content to a .txt file."""
+    with open(f'{file_name}.txt', 'w') as f:
+        f.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    """Append append_content to a .txt file."""
+    with open(f'{file_name}.txt', 'a') as f:
+        f.write(append_content)
 
 def read_file(file_name):
-    pass
+    """Read and return the content of a .txt file."""
+    with open(f'{file_name}.txt', 'r') as f:
+        file_content = f.read()
+    return file_content
